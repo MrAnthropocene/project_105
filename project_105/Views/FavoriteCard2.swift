e356201f1e61d6b7db43c5ed01fb373b1b85a38c
Jesus Montes <montesj4@uabc.edu.mx>
2026-02-16 10:31:37 -0800

    updates

---
+++ project_105/Views/FavoriteCard2.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteCard2.swift
+//  project_105
+//
+//  Created by Saul Herrera on 2/13/26.
+//
+
+import SwiftUI
+
+struct FavoriteCard2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteCard2()
+}
